57662667b8037d12d23113fd8e5b17c49f07fd4b
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-05-26 21:46:43 +0100

    added user schema

--- backend/config/config.db.js
+++ backend/config/config.db.js
@@ -1,3 +1,18 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect();
+module.exports = () => {
+	mongoose.connect(process.env.db_connection, {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+		useCreateIndex: true,
+	});
+
+	mongoose.connection.on('open', () => {
+		console.log('DB connection is successful');
+	});
+
+	mongoose.connection.on('error', () => {
+		console.log('Connection failed');
+	});
+};
