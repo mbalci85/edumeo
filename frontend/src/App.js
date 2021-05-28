229679a1c32882cfc71ac23dedeb94f198827b3a
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-05-28 20:23:09 +0100

    created reg form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,37 @@ import './App.css';
 
 const App = () => {
 	return (
-		<div>
+		<div className="container">
 			<h1>Registration Form</h1>
+			<form className="sign-up-form">
+				<label html for="fullname">
+					Full Name
+				</label>
+				<input id="fullname" placeholder="Enter your full name" />
+				<label html for="email">
+					Email
+				</label>
+				<input id="email" placeholder="Enter your email" />
+				<label html for="password">
+					Password
+				</label>
+				<input id="fullname" type="password" placeholder="Enter password" />
+				<label html for="confirm-password">
+					Confirm Password
+				</label>
+				<input
+					id="confirm-password"
+					type="password"
+					placeholder="Confirm password"
+				/>
+				<button type="submit" className="register-btn">
+					Register
+				</button>
+				<p>
+					If you have already registered, click{' '}
+					<span className="log-in-link">Log In</span>
+				</p>
+			</form>
 		</div>
 	);
 };
