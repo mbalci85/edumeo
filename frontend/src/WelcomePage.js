f6fe942fe855b59600a9587b983b7728216cee1d
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-06-04 16:05:48 +0100

    added post form to the user dashboard

--- frontend/src/WelcomePage.js
+++ frontend/src/WelcomePage.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import './App.css';
 
 const WelcomePage = ({ welcomeMessage, pageLinks, logIn, dashboardLink }) => {
 	const [name, setName] = useState('');
+	const [title, setTitle] = useState('');
+	const [body, setBody] = useState('');
 	const userInfo = JSON.parse(localStorage.getItem('userInfo'));
 
 	useEffect(() => {
@@ -14,7 +17,34 @@ const WelcomePage = ({ welcomeMessage, pageLinks, logIn, dashboardLink }) => {
 		dashboardLink(false);
 	}, [welcomeMessage, pageLinks, userInfo, logIn, dashboardLink]);
 
-	return <div className="dashboard-welcome-msg">Welcome {name}</div>;
+	return (
+		<div>
+			<div className="dashboard-welcome-msg">
+				<h4>Welcome {name}</h4>
+			</div>
+
+			<div>
+				<form className="post-form">
+					<label htmlFor="title">Title</label>
+					<input
+						id="title"
+						placeholder="Enter your title..."
+						type="text"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+					<label htmlFor="body">Post</label>
+					<textarea
+						id="body"
+						placeholder="Enter your post body..."
+						type="text"
+						value={body}
+						onChange={(e) => setBody(e.target.value)}
+					/>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default WelcomePage;
