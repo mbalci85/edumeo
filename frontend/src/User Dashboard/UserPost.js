bf482c5a4eb2132794b4a64f0ef9bd5720eab959
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-06-07 06:14:25 +0100

    added modal to read entire post

--- frontend/src/User Dashboard/UserPost.js
+++ frontend/src/User Dashboard/UserPost.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import Modal from 'react-modal';
+Modal.setAppElement('#root');
 
 const UserPost = ({ post }) => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
 	const deletePost = (id) => {
 		axios
 			.delete(`http://localhost:5000/posts/delete/${id}`)
@@ -18,12 +22,45 @@ const UserPost = ({ post }) => {
 		<div className="dashboard-all-posts-container">
 			<div className="dashboard-post-card-container">
 				<h3 className="dashboard-post-card-title">{post.title}</h3> <br />
-				<p>{post.body.split(' ').slice(0, 30).join(' ')}.....</p>
+				<p>{post.body.split(' ').slice(0, 29).join(' ')}</p>
 				<br />
-				<div className="dashboard-post-card-btn-container">
-					<button type="button" className="dashboard-post-card-btn">
-						See Details
+				{post.body.split(' ').length > 29 ? (
+					<button
+						type="button"
+						className="dashboard-post-card-btn"
+						onClick={() => setIsModalOpen(true)}
+					>
+						Read All
 					</button>
+				) : null}
+				<Modal
+					isOpen={isModalOpen}
+					onRequestClose={() => setIsModalOpen(false)}
+					style={{
+						overlay: {
+							backgroundColor: '#f5f5f5',
+						},
+						content: {
+							width: '60%',
+							height: '75%',
+							margin: 'auto',
+							padding: '30px',
+							lineHeight: '1.6',
+						},
+					}}
+				>
+					<h3 className="read-all-modal-title">{post.title}</h3>
+					<h5>{post.body}</h5>
+					<div className="read-all-modal-go-back-btn-container">
+						<button
+							className="read-all-modal-go-back-btn"
+							onClick={() => setIsModalOpen(false)}
+						>
+							Go Back
+						</button>
+					</div>
+				</Modal>
+				<div className="dashboard-post-card-btn-container">
 					<button type="button" className="dashboard-post-card-btn">
 						Update
 					</button>
