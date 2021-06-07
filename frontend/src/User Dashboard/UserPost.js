8564d0530a8fa0e2c11ec4963edc853b3c0a5db6
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-06-07 09:23:18 +0100

    edited update modal and added publish chekbox functionality

--- frontend/src/User Dashboard/UserPost.js
+++ frontend/src/User Dashboard/UserPost.js
@@ -4,7 +4,45 @@ import Modal from 'react-modal';
 Modal.setAppElement('#root');
 
 const UserPost = ({ post }) => {
-	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [isReadAllModalOpen, setIsReadAllModalOpen] = useState(false);
+	const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
+	const [newTitle, setNewTitle] = useState(post.title);
+	const [newBody, setNewBody] = useState(post.body);
+	const [updateMsg, setUpdateMsg] = useState(false);
+
+	const postId = post._id;
+
+	console.log(post.isPublished);
+
+	const publishPost = async () => {
+		const publishedPost = {
+			isPublished: !post.isPublished,
+		};
+		await axios
+			.put(`http://localhost:5000/posts/${postId}`, publishedPost)
+			.then((res) => res.data)
+			.catch((err) => console.log(err));
+		window.location.reload();
+	};
+
+	const updatePost = async (e) => {
+		e.preventDefault();
+		const newPost = {
+			title: newTitle,
+			body: newBody,
+		};
+		await axios
+			.put(`http://localhost:5000/posts/${postId}`, newPost)
+			.then((res) => res.data)
+			.catch((err) => console.log(err));
+		setUpdateMsg(true);
+		setTimeout(() => {
+			setIsUpdateModalOpen(false);
+		}, 2000);
+		setTimeout(() => {
+			window.location.reload();
+		}, 2001);
+	};
 
 	const deletePost = (id) => {
 		axios
@@ -22,20 +60,22 @@ const UserPost = ({ post }) => {
 		<div className="dashboard-all-posts-container">
 			<div className="dashboard-post-card-container">
 				<h3 className="dashboard-post-card-title">{post.title}</h3> <br />
-				<p>{post.body.split(' ').slice(0, 29).join(' ')}</p>
+				<p className="dashboard-post-card-body">
+					{post.body.split(' ').slice(0, 24).join(' ')}
+				</p>
 				<br />
 				{post.body.split(' ').length > 29 ? (
 					<button
 						type="button"
 						className="dashboard-post-card-btn"
-						onClick={() => setIsModalOpen(true)}
+						onClick={() => setIsReadAllModalOpen(true)}
 					>
 						Read All
 					</button>
 				) : null}
 				<Modal
-					isOpen={isModalOpen}
-					onRequestClose={() => setIsModalOpen(false)}
+					isOpen={isReadAllModalOpen}
+					onRequestClose={() => setIsReadAllModalOpen(false)}
 					style={{
 						overlay: {
 							backgroundColor: '#f5f5f5',
@@ -44,26 +84,73 @@ const UserPost = ({ post }) => {
 							width: '60%',
 							height: '75%',
 							margin: 'auto',
-							padding: '30px',
+							padding: '30px 50px',
 							lineHeight: '1.6',
+							textAlign: 'justify',
 						},
 					}}
 				>
 					<h3 className="read-all-modal-title">{post.title}</h3>
-					<h5>{post.body}</h5>
+					<p>{post.body}</p>
 					<div className="read-all-modal-go-back-btn-container">
 						<button
 							className="read-all-modal-go-back-btn"
-							onClick={() => setIsModalOpen(false)}
+							onClick={() => setIsReadAllModalOpen(false)}
 						>
 							Go Back
 						</button>
 					</div>
 				</Modal>
 				<div className="dashboard-post-card-btn-container">
-					<button type="button" className="dashboard-post-card-btn">
+					<button
+						type="button"
+						className="dashboard-post-card-btn"
+						onClick={() => setIsUpdateModalOpen(true)}
+					>
 						Update
 					</button>
+					<Modal
+						isOpen={isUpdateModalOpen}
+						onRequestClose={() => setIsUpdateModalOpen(false)}
+						style={{
+							overlay: {
+								backgroundColor: '#f5f5f5',
+							},
+							content: {
+								width: '60%',
+								height: '75%',
+								margin: 'auto',
+								padding: '30px 50px',
+								lineHeight: '1.6',
+								textAlign: 'justify',
+							},
+						}}
+					>
+						<form
+							className="update-post-modal-form"
+							onSubmit={updatePost}
+						>
+							<input
+								value={newTitle}
+								onChange={(e) => setNewTitle(e.target.value)}
+							/>
+							<textarea
+								value={newBody}
+								onChange={(e) => setNewBody(e.target.value)}
+							/>
+							<button
+								type="submit"
+								className="update-post-modal-form-btn"
+							>
+								Update
+							</button>
+							{updateMsg ? (
+								<small className="update-post-modal-form-success-msg">
+									You have updated your post successfully
+								</small>
+							) : null}
+						</form>
+					</Modal>
 					<button
 						type="button"
 						className="dashboard-post-card-btn"
@@ -71,8 +158,13 @@ const UserPost = ({ post }) => {
 					>
 						Delete
 					</button>
-					<input type="checkbox" id="checkbox" />
-					<label htmlFor="checkbox">Publish</label>
+					<input
+						type="checkbox"
+						id="checkbox"
+						checked={post.isPublished}
+						onChange={() => publishPost()}
+					/>
+					<label htmlFor="checkbox">Publish</label> <br />
 				</div>
 			</div>
 		</div>
