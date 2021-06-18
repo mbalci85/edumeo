3a72c6b278f021c49f404b93dc39eabdada7cee5
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-06-18 14:25:55 +0100

    updated user model

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -3,7 +3,9 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
 	{
-		fullName: { type: String, required: true },
+		username: { type: String, required: true, unique: true },
+		firstname: { type: String, required: true },
+		lastname: { type: String, required: true },
 		email: { type: String, required: true, unique: true, lowercase: true },
 		password: { type: String, required: true },
 		role: { type: String, default: 'user' },
