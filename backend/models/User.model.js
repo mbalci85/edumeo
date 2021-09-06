bf8686fa3573ecd4b5a7f9ff405df661bc499813
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-09-06 23:03:54 +0100

    updated user model

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -5,7 +5,7 @@ const UserSchema = new Schema(
 	{
 		username: { type: String, required: true, unique: true },
 		firstname: String,
-		lastname: { type: String },
+		lastname: String,
 		email: { type: String, required: true, unique: true, lowercase: true },
 		password: { type: String, required: true },
 		role: { type: String, default: 'user' },
