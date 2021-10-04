c06034b2030e22af6986d786730f6b7e24563166
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-10-04 23:21:06 +0100

    updated admin.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Edumeo' });
+	res.render('index', { title: 'Edumeo!!!' });
 });
 
 module.exports = router;
